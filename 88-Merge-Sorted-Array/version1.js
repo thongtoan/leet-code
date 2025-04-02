00b8e948f0f71a42fa24d56c7932cf9827c84a09
Thong Toan <thongtoan.nguyen@stunited.vn>
2025-04-02 20:49:51 +0700

    Add and Finish 88 Merge Sorted Array

---
+++ 88-Merge-Sorted-Array/version1.js
@@ -0,0 +1,32 @@
+let nums1 = [1, 2, 3, 0, 0, 0];
+let nums2 = [2, 5, 6];
+let nums = [];
+let m = 3;
+let n = 3;
+
+var merge = function(nums1, m, nums2, n) {
+    nums1.splice(m);
+    nums2.splice(n); 
+    let i = 0;
+    let j = 0;
+    while (i < nums1.length) {
+        while (j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                nums.push(nums1[i]);
+                i++;
+            } else if (nums1[i] === nums2[j]) {
+                nums.push(nums1[i], nums2[j]);
+                i++, j++;
+            } else {
+                nums.push(nums2[j]);
+                j++;
+            }
+        };
+    };
+    if (i === nums1.length || j === nums2.length) {
+        nums.push()
+    }
+    return nums;
+};
+
+console.log(merge(nums1, m, nums2, n));
---
+++ 88-Merge-Sorted-Array/version3.js
@@ -0,0 +1,33 @@
+let nums1 = [1, 2, 3, 0, 0, 0];
+let nums2 = [2, 5, 6];
+let nums = [];
+let m = 3;
+let n = 3;
+
+var merge = function(nums1, m, nums2, n) {
+    let i = m - 1;
+    let j = n - 1;
+    let k = m + n - 1;
+    while (i >= 0 && j >=0) {
+         if (nums1[i] < nums2[j]) {
+             nums1[k] = nums2[j];
+             j--;
+             k--;
+         } else if (nums1[i] > nums2[j]) {
+             nums1[k] = nums1[i];
+             i--;
+             k--;
+         } else {
+             nums1[k] = nums2[j];
+             k--;
+         }
+    }
+    while (j >= 0) {
+         nums1[k] = nums2[j];
+         j--;
+         k--;
+    }
+    return nums1;
+};
+
+console.log(merge(nums1, m, nums2, n));
